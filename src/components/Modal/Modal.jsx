fc690074a41a27add15e967d35623690e2c8e574
himanshu8443 <sangwanhimanshu8443@gmail.com>
2023-09-23 14:22:49 +0530

    Invitation cost

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,14 +6,14 @@ import { IoCloseSharp } from 'react-icons/io5'
 
 const Modal = ({ setShowModal }) => {
     const basePrice = 10;
-    const durationMultiplier = 2;
+    const durationMultiplier = 5;
     const [noOfInvites, setNoOfInvites] = useState(1)
     const [duration, setDuration] = useState(30)
     const [cost, setCost] = useState(basePrice * noOfInvites + durationMultiplier * duration)
     return (
         <div className=' fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center overflow-scroll'>
-            <div className=' bg-white p-4 rounded-md shadow-md overflow-scrolls'>
-                <div className=' flex gap-4 justify-between items-center w-[80vw]  md:w-[700px] overflow-scroll'>
+            <div className=' bg-white p-4 rounded-md shadow-md overflow-scrolls w-[90vw]  md:w-[700px]'>
+                <div className=' flex gap-4 justify-between items-center'>
                     <h1 className=' text-3xl font-bold'>Invite your team</h1>
                     <button
                         onClick={() => { setShowModal(false) }}
@@ -28,7 +28,7 @@ const Modal = ({ setShowModal }) => {
                         <div className='flex flex-col'>
                             <div className='flex gap-7 justify-between items-center'>
                                 <label className=' text-xl font-medium whitespace-nowrap'>No. of Invites</label>
-                                <p className=' text-xl font-medium mr-5 border-dotted border-gray-400 border-2 px-2 py-1 rounded-md'>{noOfInvites}</p>
+                                <p className=' min-w-[50px] text-center text-xl font-medium mr-5 border-dotted border-gray-400 border-2 px-2 py-1 rounded-md'>{noOfInvites}</p>
                             </div>
                             <input type='range' min={1} max={500} value={noOfInvites}
                                 onChange={(e) => { setNoOfInvites(e.target.value); setCost(e.target.value * basePrice + durationMultiplier * duration) }}
@@ -37,7 +37,7 @@ const Modal = ({ setShowModal }) => {
                         <div className='flex flex-col'>
                             <div className='flex gap-7 justify-between items-center'>
                                 <label className=' text-xl font-medium whitespace-nowrap'>Duration</label>
-                                <p className=' text-xl font-medium mr-5 border-dotted border-gray-400 border-2 px-2 py-1 rounded-md'>{duration} min</p>
+                                <p className='min-w-[100px] text-center text-xl font-medium mr-5 border-dotted border-gray-400 border-2 px-2 py-1 rounded-md'>{duration} min</p>
                             </div>
                             <input type='range' min={30} max={240} value={duration}
                                 onChange={(e) => { setDuration(e.target.value); setCost(e.target.value * durationMultiplier + noOfInvites * basePrice) }}
@@ -46,11 +46,11 @@ const Modal = ({ setShowModal }) => {
                         <div className='flex flex-col gap-2 ml-4'>
                             <div className=' gap-8 justify-between items-center hidden md:flex'>
                                 <label className=' text-lg font-medium whitespace-nowrap'>Duration Cost</label>
-                                <p className=' text-lg font-medium mr-5'>₹ {durationMultiplier * duration}</p>
+                                <p className=' text-lg font-medium mr-5'>₹ {durationMultiplier} X {duration}</p>
                             </div>
                             <div className=' gap-8 justify-between items-center hidden md:flex'>
                                 <label className=' text-lg font-medium whitespace-nowrap'>Invitation Cost</label>
-                                <p className=' text-lg font-medium mr-5'>₹ {basePrice * noOfInvites}</p>
+                                <p className=' text-lg font-medium mr-5'>₹ {basePrice} X {noOfInvites}</p>
                             </div>
                             <div className='flex gap-8 justify-between items-center'>
                                 <label className=' text-xl font-medium whitespace-nowrap '>Total Cost</label>
